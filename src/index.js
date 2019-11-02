1553c7601d66fa8152abdade94818c92e217b79f
Roman Vozbrannyi <romanvozbrannyi@gmail.com>
2019-11-02 10:28:47 +0200

    Sass setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 console.log(`The time is ${new Date()}`);
+import './scss/main.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: './src/index.js',
@@ -23,7 +24,19 @@ module.exports = {
                     },
                 },
             },
-            // ...additional rules...
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
         ],
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/[name].css",
+        }),
+    ],
 };
